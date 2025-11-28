bd38bb016341d96f52bf863c8566e667cbfdfadf
swords02 <owengerke@icloud.com>
2025-11-28 15:36:00 -0600

    Add files via upload

--- play/DartConfiguration.tcl
+++ play/DartConfiguration.tcl
@@ -11,7 +11,7 @@ BuildDirectory: /src/build
 CostDataFile: 
 
 # Site is something like machine.domain, i.e. pragmatic.crd
-Site: 81ace13f3a46
+Site: 7ac104f5e471
 
 # Build name is osname-revision-compiler, i.e. Linux-2.4.2-2smp-c++
 BuildName: Emscripten-em++
